a59f25017ce50d5f7939fc1b7520db4c1b30ad22
Alex <45456278+pan-narrans@users.noreply.github.com>
2022-10-24 09:15:03 +0200

    Ejercicio 03 completado

--- js/ejercicio03.js
+++ js/ejercicio03.js
@@ -1,25 +1,52 @@
-
+// Event listeners + body
 const body = document.getElementsByTagName("body")[0];
-body.addEventListener("keydown", showKey);
 body.addEventListener("click", showClick);
+body.addEventListener("mousemove", showMove);
+body.addEventListener("keydown", showKey);
 
+// References
+// Move
+const mMoveScreen = document.getElementById("mMoveScreen");
+const mMovePage = document.getElementById("mMovePage");
+const mMoveClient = document.getElementById("mMoveClient");
 
-
-const mClickBrowser = document.getElementById("mClickBrowser");
+// Click
+const mClick = document.getElementById("mClick");
+const mClickScreen = document.getElementById("mClickScreen");
 const mClickPage = document.getElementById("mClickPage");
+const mClickClient = document.getElementById("mClickClient");
 
-function showClick(e) {
-  mClickBrowser.innerHTML = `
-  Screen X/Y: ${e.screenX}, ${e.screenY}
-  Page X/Y: ${e.pageX}, ${e.pageY}
-  Client X/Y: ${e.clientX}, ${e.clientY}`
-  mClickPage.innerHTML = "[" + e.screenX + "]"
-}
-
+// Keyboard
+const keyboard = document.getElementById("keyboard");
 const char = document.getElementById("char");
 const code = document.getElementById("code");
 
+// Functions
+function showMove(e) {
+  // Return to white background
+  mClick.classList.remove('infoYellow');
+  keyboard.classList.remove('infoBlue');
+
+  mMoveScreen.innerHTML = `[ ${e.screenX}, ${e.screenY} ]`;
+  mMovePage.innerHTML = `[ ${e.pageX}, ${e.pageY} ]`;
+  mMoveClient.innerHTML = `[ ${e.clientX}, ${e.clientY} ]`;
+}
+
+function showClick(e) {
+  // Background color change
+  mClick.classList.add('infoYellow');
+
+  // Show info
+  mClickScreen.innerHTML = `[ ${e.screenX}, ${e.screenY} ]`;
+  mClickPage.innerHTML = `[ ${e.pageX}, ${e.pageY} ]`;
+  mClickClient.innerHTML = `[ ${e.clientX}, ${e.clientY} ]`;
+}
+
 function showKey(e) {
-  char.innerHTML = "[" + e.key + "]"
-  code.innerHTML = "[" + e.code + "]"
+  // Background color change
+  keyboard.classList.add('infoBlue');
+
+  // Show info
+  char.innerHTML = `"[ ${e.key} ]"`
+  code.innerHTML = `"[ ${e.code} ]"`
 }
